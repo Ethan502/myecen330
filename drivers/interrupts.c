6b11ff727bf1ebd41a4626fa61da7d6daac2565c
Ethan Smith <esmit502@DIGITAL-56.ee.byu.edu>
2022-09-28 21:55:21 -0600

    completed lab 4

--- drivers/interrupts.c
+++ drivers/interrupts.c
@@ -1,8 +1,8 @@
+#include "interrupts.h"
+#include "armInterrupts.h"
 #include <stdio.h>
 #include <xil_io.h>
 #include <xparameters.h>
-#include "armInterrupts.h"
-#include "interrupts.h"
 
 #define IPR_OFFSET 0x04
 #define IER_OFFSET 0x08
@@ -13,67 +13,62 @@
 
 #define MER_INIT_VAL 0x03
 #define ZEROS 0x00
+#define ZERO_INDEX 0
+#define SHIFT_NUM 1
 
 #define NUM_OF_FCN_PTRS 3
 
+// global variable for the array of function pointers
 static void (*isrFcnPtrs[NUM_OF_FCN_PTRS])() = {NULL};
 
-//function to read the regester given the offset
-static uint32_t readRegister(uint32_t offset)
-{
+// function to read the regester given the offset
+static uint32_t readRegister(uint32_t offset) {
   return Xil_In32(XPAR_AXI_INTC_0_BASEADDR + offset);
 }
 
 // function to write the given 32bit uint (value) to the register
-static void writeRegister(uint32_t offset, uint32_t value) 
-{
+static void writeRegister(uint32_t offset, uint32_t value) {
   Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + offset, value);
 }
 
-static void interrupts_isr()
-{
-    int arraySize = sizeof(isrFcnPtrs)/sizeof(isrFcnPtrs[0]);
-    sizeof(isrFcnPtrs)/sizeof(isrFcnPtrs[0]);
-    for(int i = 0; i < arraySize; i++)
-    {
-        //see if a function has an interrupt pending
-        if(readRegister(IPR_OFFSET) & (1 << i))
-        {
-            //check if there is a callback
-            if(isrFcnPtrs[i] != NULL)
-            {
-                isrFcnPtrs[i]();
-            }
-            //acknowledge the interrupt
-            writeRegister(IAR_OFFSET,1<<i);
-        }
+// go through the fcnptr list and activate them if they are still pending
+static void interrupts_isr() {
+  int arraySize = sizeof(isrFcnPtrs) / sizeof(isrFcnPtrs[ZERO_INDEX]);
+  sizeof(isrFcnPtrs) / sizeof(isrFcnPtrs[ZERO_INDEX]);
+  for (int i = ZERO_INDEX; i < arraySize; i++) {
+    // see if a function has an interrupt pending
+    if (readRegister(IPR_OFFSET) & (SHIFT_NUM << i)) {
+      // check if there is a callback
+      if (isrFcnPtrs[i] != NULL) {
+        isrFcnPtrs[i]();
+      }
+      // acknowledge the interrupt
+      writeRegister(IAR_OFFSET, SHIFT_NUM << i);
     }
+  }
 }
 
-//initialize the interrupt controller
-void interrupts_init()
-{
-    print("in the init\n");
-    writeRegister(MER_OFFSET,MER_INIT_VAL); //enable the interrupt output
-    writeRegister(IER_OFFSET, ZEROS); //disables all interrupt input lines
+// initialize the interrupt controller
+void interrupts_init() {
+  writeRegister(MER_OFFSET, MER_INIT_VAL); // enable the interrupt output
+  writeRegister(IER_OFFSET, ZEROS);        // disables all interrupt input lines
 
-    armInterrupts_init();
-    armInterrupts_setupIntc(interrupts_isr);
-    armInterrupts_enable();
+  armInterrupts_init();
+  armInterrupts_setupIntc(interrupts_isr);
+  armInterrupts_enable();
+
+  writeRegister(IER_OFFSET, ZEROS);
 }
 
-//store a function in the isr
-void interrupts_register(uint8_t irq, void (*fcn)())
-{
-    isrFcnPtrs[irq] = fcn;
+// store a function in the isr
+void interrupts_register(uint8_t irq, void (*fcn)()) { isrFcnPtrs[irq] = fcn; }
+
+// enable the irq
+void interrupts_irq_enable(uint8_t irq) {
+  writeRegister(SIE_OFFSET, SHIFT_NUM << irq);
 }
 
-void interrupts_irq_enable(uint8_t irq)
-{
-    writeRegister(SIE_OFFSET, 1 << irq);
-}
-
-void interrupts_irq_disable(uint8_t irq)
-{
-    writeRegister(CIE_OFFSET, 1 << irq);
+// disable the irq
+void interrupts_irq_disable(uint8_t irq) {
+  writeRegister(CIE_OFFSET, SHIFT_NUM << irq);
 }
--- lab4_interrupts/interrupt_test.c
+++ lab4_interrupts/interrupt_test.c
@@ -1,48 +1,63 @@
-#include "leds.h"
+#include "interrupt_test.h"
 #include "interrupts.h"
 #include "intervalTimer.h"
-#include "interrupt_test.h"
+#include "leds.h"
 
-bool on = false;
+#define LED_0_MASK 0x01
+#define LED_1_MASK 0x02
+#define LED_2_MASK 0x04
 
-static void myIsr()
-{
-if(on)
-{
-   leds_write(0x0000);
-   on = false;
-}
-else if(!on)
-{
-    leds_write(0x0001);
-    on = true;
-}
-intervalTimer_ackInterrupt(INTERVAL_TIMER_0);
+#define TENHERTZ 0.1
+#define ONEHERTZ 1
+#define TENTHHERTZ 10
+
+// function to blink the lights on the 0 timer
+static void timer0_isr() {
+  leds_write(leds_read() ^ LED_0_MASK);
+  intervalTimer_ackInterrupt(INTERVAL_TIMER_0);
 }
 
-// void interrupt_test_run() 
-// {
-// interrupts_init();
-// interrupts_register(INTERVAL_TIMER_0_INTERRUPT_IRQ, myIsr);
-// interrupts_irq_enable(INTERVAL_TIMER_0_INTERRUPT_IRQ);
-// intervalTimer_initCountDown(INTERVAL_TIMER_0, 1);
-// intervalTimer_enableInterrupt(INTERVAL_TIMER_0);
-// intervalTimer_start(INTERVAL_TIMER_0);
-// while (1);
-// }
+// function to blink the lights on the timer 1
+static void timer1_isr() {
+  leds_write(leds_read() ^ LED_1_MASK);
+  intervalTimer_ackInterrupt(INTERVAL_TIMER_1);
+}
 
+// function to blink the lights on the timer 2
+static void timer2_isr() {
+  leds_write(leds_read() ^ LED_2_MASK);
+  intervalTimer_ackInterrupt(INTERVAL_TIMER_2);
+}
 
-void interrupt_test_run()
-{
-    leds_init();
-    interrupts_init();
+// this is the main function to test my interrupts and blink the lights
+void interrupt_test_run() {
+  leds_init();
+  interrupts_init();
 
-    interrupts_register(INTERVAL_TIMER_0_INTERRUPT_IRQ,myIsr);
-    interrupts_irq_enable(INTERVAL_TIMER_0_INTERRUPT_IRQ);
+  // do stuff for the LED, blink at 10 Hz
+  interrupts_register(INTERVAL_TIMER_0_INTERRUPT_IRQ, timer0_isr);
+  interrupts_irq_enable(INTERVAL_TIMER_0_INTERRUPT_IRQ);
 
-    intervalTimer_initCountDown(INTERVAL_TIMER_0,1);
-    intervalTimer_enableInterrupt(INTERVAL_TIMER_0);
-    intervalTimer_start(INTERVAL_TIMER_0);
+  intervalTimer_initCountDown(INTERVAL_TIMER_0, TENHERTZ);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_0);
+  intervalTimer_start(INTERVAL_TIMER_0);
 
-    while(1);
+  // do stuff for the LED 1, blink at 1 Hz
+  interrupts_register(INTERVAL_TIMER_1_INTERRUPT_IRQ, timer1_isr);
+  interrupts_irq_enable(INTERVAL_TIMER_1_INTERRUPT_IRQ);
+
+  intervalTimer_initCountDown(INTERVAL_TIMER_1, ONEHERTZ);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_1);
+  intervalTimer_start(INTERVAL_TIMER_1);
+
+  // do stuff for the LED 2, blink at .1 Hz
+  interrupts_register(INTERVAL_TIMER_2_INTERRUPT_IRQ, timer2_isr);
+  interrupts_irq_enable(INTERVAL_TIMER_2_INTERRUPT_IRQ);
+
+  intervalTimer_initCountDown(INTERVAL_TIMER_2, TENTHHERTZ);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_2);
+  intervalTimer_start(INTERVAL_TIMER_2);
+
+  while (1)
+    ;
 }
